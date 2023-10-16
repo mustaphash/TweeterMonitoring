96ee27474c0fe1a4f2f5c388e7add301f22ed938
Mustafa Shakir <mshakir9918@gmail.com>
2023-10-16 17:00:09 +0300

    Add UpdateCommands

---
+++ src/DAL/Commands/UpdateAccountsCommands/UpdateTwitterAccountCommand.cs
@@ -0,0 +1,18 @@
+﻿using Core.Commands;
+using Core.Entities;
+
+namespace DAL.Commands.UpdateAccountsCommands
+{
+    public class UpdateTwitterAccountCommand : ICommand
+    {
+        public UpdateTwitterAccountCommand(string userName, TwitterAccount twitterAccount)
+        {
+            UserName = userName;
+            TwitterAccount = twitterAccount;
+        }
+
+        public string UserName { get; set; }
+
+        public TwitterAccount TwitterAccount { get; set; }
+    }
+}
---
+++ src/DAL/Commands/UpdateAccountsCommands/UpdateTwitterAccountCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Core.Commands;
+
+namespace DAL.Commands.UpdateAccountsCommands
+{
+    public class UpdateTwitterAccountCommandHandler : ICommandHandler<UpdateTwitterAccountCommand>
+    {
+        private readonly TwitterContext _twitterContext;
+
+        public UpdateTwitterAccountCommandHandler(TwitterContext twitterContext)
+        {
+            _twitterContext = twitterContext;
+        }
+
+        public async Task HandleAsync(UpdateTwitterAccountCommand command, CancellationToken cancellationToken = default)
+        {
+            var twitterAccounts = _twitterContext.TwitterAccounts.FirstOrDefault(t => t.UserName == command.UserName);
+
+            if (twitterAccounts != null)
+            {
+                twitterAccounts.Url = command.TwitterAccount.Url;
+                twitterAccounts.TweetId = command.TwitterAccount.TweetId;
+                twitterAccounts.TweetDate = command.TwitterAccount.TweetDate;
+
+                await _twitterContext.SaveChangesAsync();
+            }
+        }
+    }
+}
